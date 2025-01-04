d777121b47a8c549958e517f81d35c9eaf5490f1
Luminaire1337 <16178449+Luminaire1337@users.noreply.github.com>
2025-01-04 20:40:00 +0100

    Initial commit

---
+++ src/main/java/io/github/luminaire1337/swimfacilityapi/SwimFacilityApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.luminaire1337.swimfacilityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimFacilityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwimFacilityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/luminaire1337/swimfacilityapi/SwimFacilityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luminaire1337.swimfacilityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimFacilityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
